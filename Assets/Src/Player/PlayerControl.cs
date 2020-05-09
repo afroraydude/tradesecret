bb7fa59bde7ee1c68719c784244555e3e8216c67
afroraydude <thomasray0@gmail.com>
2020-05-09 18:15:32 -0400

    redo model + animations

--- Assets/Src/Player/PlayerControl.cs
+++ Assets/Src/Player/PlayerControl.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+            anim.SetBool("WalkStarted", true);
     }
 }
