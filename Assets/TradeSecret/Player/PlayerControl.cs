c441110c1faf8a98b65d8758b31623441752708a
afroraydude <thomasray0@gmail.com>
2020-07-23 18:36:54 -0400

    New  player animations

--- Assets/TradeSecret/Player/PlayerControl.cs
+++ Assets/TradeSecret/Player/PlayerControl.cs
@@ -21,57 +21,21 @@ namespace TradeSecret.Player
 
             float speed = defaultSpeed;
 
-            // bad if chain
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) ||
+                Input.GetKey(KeyCode.D))
             {
                 anim.SetBool("Walking", true);
-
-                anim.SetBool("Forward", true);
             }
-            else if (Input.GetKeyUp(KeyCode.W))
+            else
             {
                 anim.SetBool("Walking", false);
-                anim.SetBool("Forward", false);
-            }
-
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                anim.SetBool("Walking", true);
-                anim.SetBool("Left", true);
-            }
-            else if (Input.GetKeyUp(KeyCode.A))
-            {
-                anim.SetBool("Walking", false);
-                anim.SetBool("Left", false);
-            }
-
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                anim.SetBool("Walking", true);
-                anim.SetBool("Back", true);
-
-                speed /= 2;
-            }
-            else if (Input.GetKeyUp(KeyCode.S))
-            {
-                anim.SetBool("Walking", false);
-                anim.SetBool("Back", false);
-            }
-
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                anim.SetBool("Walking", true);
-                anim.SetBool("Right", true);
-            }
-            else if (Input.GetKeyUp(KeyCode.D))
-            {
-                anim.SetBool("Walking", false);
-                anim.SetBool("Right", false);
             }
 
             // Translation of stuff aka acceleration and speed
             float translationX = Input.GetAxis("Horizontal") * speed;
+            anim.SetFloat("VelocityX", translationX);
             float translationZ = Input.GetAxis("Vertical") * speed;
+            anim.SetFloat("VelocityZ", translationZ);
             translationX *= Time.deltaTime;
             translationZ *= Time.deltaTime;
 
