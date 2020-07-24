26a0984eec753eb3841498840b0e4522bfa9868d
afroraydude <thomasray0@gmail.com>
2020-07-24 12:57:40 -0400

    Asset and script additions

---
+++ Assets/TradeSecret/UI/DisplayVersion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayVersion : MonoBehaviour
+{
+    private Text versionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        versionText = GetComponentInParent<Text>();
+        versionText.text = $"Pre-Alpha v{Application.version}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
